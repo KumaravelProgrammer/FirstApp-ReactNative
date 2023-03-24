5e3be979c9754c9c5b68f0f5811a7fbd5347ed36
KumaravelProgrammer <kumaravelprogrammer@gmail.com>
2023-03-24 18:34:28 +0530

    first commit

---
+++ LoginScreeen.js
@@ -0,0 +1,66 @@
+// LoginScreen.js
+
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import CourseScreen from './CourseScreen';
+
+const LoginScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigation = useNavigation();
+
+  const handleLogin = () => {
+    // Check if the username and password are correct
+    if (username === 'admin' && password === 'admin123') {
+
+      navigation.navigate('Courses');
+    } else {
+      alert('Invalid username or password');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={(text) => setUsername(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
+  },
+});
+
+export default LoginScreen;
